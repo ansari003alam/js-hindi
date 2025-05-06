21f02c95beb2e883572d323c542f1eb879911c41
Md Saif Ansari <saif003alam@gmail.com>
2025-05-06 15:06:34 +0000

    Dates

--- 01_basics/dates.js
+++ 01_basics/dates.js
@@ -1,22 +1,22 @@
 let curdate = new Date();
-// console.log(curdate.toString());
-// console.log(curdate.toLocaleString());
-// console.log(curdate.toDateString());
-// console.log(curdate.toLocaleDateString());
-// console.log(curdate.toJSON());
-// console.log(curdate.toISOString());
-// console.log(typeof curdate);
+console.log(curdate.toString());
+console.log(curdate.toLocaleString());
+console.log(curdate.toDateString());
+console.log(curdate.toLocaleDateString());
+console.log(curdate.toJSON());
+console.log(curdate.toISOString());
+console.log(typeof curdate);
 
 let myCreatedDate = new Date(2003, 7, 31, 8, 30)
 
-//console.log(myCreatedDate.toLocaleString());
+console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now();
 
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
 
-//console.log(Math.floor(Date.now()/1000));
+console.log(Math.floor(Date.now()/1000));
 
 let myDate = new Date()
 
