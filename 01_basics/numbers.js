2c33d02932525415f30b409ac8511779c888f2c1
Md Saif Ansari <saif003alam@gmail.com>
2025-05-10 16:39:56 +0000

    Array

--- 01_basics/numbers.js
+++ 01_basics/numbers.js
@@ -1,34 +1,34 @@
-// const score  = 400;
-// console.log(score);
+const score  = 400;
+console.log(score);
 
-// const balance = new Number(100);
-// console.log(balance);
+const balance = new Number(100);
+console.log(balance);
 
-// console.log(balance.toLocaleString());
-// console.log(balance.toLocaleString().length);
+console.log(balance.toLocaleString());
+console.log(balance.toLocaleString().length);
 
-// console.log(balance.toFixed(4));
+console.log(balance.toFixed(4));
 
-// const anotherNumber = 123.8966;
+const anotherNumber = 123.8966;
 
-// console.log(anotherNumber.toPrecision(3));
+console.log(anotherNumber.toPrecision(3));
 
-// const hundred  = 1000000;
-// console.log(hundred.toLocaleString('en-IN'));
+const hundred  = 1000000;
+console.log(hundred.toLocaleString('en-IN'));
 
-// console.log(Math.abs(-5));
-// console.log(Math.round(4.49));
+console.log(Math.abs(-5));
+console.log(Math.round(4.49));
 
-// console.log(Math.ceil(4.3));
-// console.log(Math.floor(4.8));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.8));
 
-// console.log(Math.sqrt(25));
+console.log(Math.sqrt(25));
 
-// console.log(Math.min(4,5,6,2,1));
+console.log(Math.min(4,5,6,2,1));
 
-// console.log(Math.max(4,5,6,2,1));
+console.log(Math.max(4,5,6,2,1));
 
-// console.log(Math.random());
+console.log(Math.random());
 
 const min = 10;
 const max = 20;
---
+++ 02_basics/array2.js
@@ -0,0 +1,32 @@
+// const bollyHeros = ["Salman", "Shahrukh"];
+
+// const bhojHeros = ["Khesari" , "Dinesh"];
+
+
+// const allHeros = bollyHeros.concat(bhojHeros);
+
+// console.log(allHeros);
+
+// const new_all_Heros = [...bollyHeros, ...bhojHeros];
+
+// console.log(new_all_Heros);
+
+
+const other_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+
+const real_array = other_array.flat(3);
+
+//console.log(real_array);
+
+console.log(Array.isArray("Ansari"));
+console.log(Array.from('Ansari'));
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
+
+
+
+
